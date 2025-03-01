0ce69cd54c2c28639059274a41e35595f620712b
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-01 10:53:57 +0300

    Update 1.sh

--- bash/1.sh
+++ bash/1.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+timeframe=$1
+file=$2
+
+while [ $timeframe -gt 0 ]
+do
+  avg=$(cat /proc/loadavg)
+  echo "$(date +'%d.%m.%y %H:%M:%S') = $avg" >> $file
+  timeframe=$(($timeframe - 1))
+  sleep 1
+done
