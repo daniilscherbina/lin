a5c750efec1066f61005dcab9b5c7b31d5f615e8
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 20:37:29 +0300

    Update 7.sh

--- bash/7.sh
+++ bash/7.sh
@@ -3,6 +3,6 @@
 path=$(echo $PATH | tr ':' '\n')
 
 echo "$path" | while IFS= read -r line; do
-    count=$(find $line -maxdepth 1 | wc -l)
+    count=$(find $line -maxdepth 1 -type f | wc -l)
     echo "$line => $count"
 done
