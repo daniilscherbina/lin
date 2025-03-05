206a756d4bc7a71549ede8af7dd70c596e879286
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 19:25:21 +0300

    Update 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+for path in "$@"; do
+    if [ -e "$path" ]; then
+        count=$(wc -l $path)
+        echo "$path => $count"
+    else
+        echo "$path => not found"
+    fi
+done
