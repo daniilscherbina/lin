e3ef5607ba8cc65c189b79cc0901feb672fb557b
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 19:11:50 +0300

    Update 2.sh

--- bash/2.sh
+++ bash/2.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+if [ ! -e "$1" ]; then
+    echo "256"
+    exit 1
+fi
+
+for dir in "$1"/*/; do
+    if [ -d "$dir" ]; then
+        count=$(find $dir -maxdepth 1 | wc -l)
+        echo "$count" >> $dir
+    fi
+done
