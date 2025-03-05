67eb7d76d87aa1e67d540f41de803032d7cba98e
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 20:18:14 +0300

    Update 8.sh

--- bash/8.sh
+++ bash/8.sh
@@ -3,7 +3,7 @@
 
 count=$(printenv | grep '^LC_' | awk -F= '{print $2}' | sort | uniq | wc -l)
 
-if [ $count -ne 1 ]; then
+if [ $count -ne 1 ] || [ $count -eq 0 ]; then
     echo "256"
     exit 1
 fi
