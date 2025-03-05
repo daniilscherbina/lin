bcf3690f5f601783813b3264b8c33db9b886d94a
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 19:30:51 +0300

    Update 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -3,6 +3,7 @@
 for path in "$@"; do
     if [ -e "$path" ]; then
         count=$(wc -l $path)
+        count=${count/" $path"/""}
         echo "$path => $count"
     else
         echo "$path => not found"
