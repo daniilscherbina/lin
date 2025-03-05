f3ddd46ca2e01f92606aa783df914ae874e9b628
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-05 18:33:55 +0300

    Update 4.sh

--- bash/4.sh
+++ bash/4.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+
+planet_name="$1"
+
+case "$planet_name" in
+    "Mercury")
+        echo "0"
+        ;;
+    "Venus")
+        echo "0"
+        ;;
+    "Earth")
+        echo "1"
+        ;;
+    "Mars")
+        echo "2"
+        ;;
+    "Jupiter")
+        echo "92"
+        ;;
+    "Saturn")
+        echo "145"
+        ;;
+    "Uranus")
+        echo "27"
+        ;;
+    "Neptune")
+        echo "14"
+        ;;
+    *)
+        echo "Unknown planet"
+        exit 1
+        ;;
+esac
