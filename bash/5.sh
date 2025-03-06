6e1dd3075dac3276d4e354fe025c2e46a40ca05e
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-06 10:16:14 +0300

    Update 5.sh

--- bash/5.sh
+++ bash/5.sh
@@ -2,5 +2,8 @@
 
 for file in $(find "/var/log" -type f -regex '.*\.log$'); do
     end_st=$(tail -n 1 $file)
+    if [ -z "${end_st}" ]; then
+        end_st=$(tail -n 2 $file | tr '\n' '')
+    fi
     echo "$end_st" >> logs.log
 done
