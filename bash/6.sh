5b25ddd633acbfb1fd72c92e0bafa3f71ffd329c
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-12 09:51:47 +0300

    Update 6.sh

--- bash/6.sh
+++ bash/6.sh
@@ -3,9 +3,14 @@
 if [[ "$FOO" -eq 5 && "$BAR" -eq 1 ]]; then
     exit 1
 fi
+count=0
 
 while [ -z "$(ls -A .)" ]; do
     sleep 1
+    count=$(($count+1))
+    if [ $count -eq 10 ]; then
+        exit 1
+    fi
 done
 
 echo "File"
