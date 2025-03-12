11a3fef21ceb8b5d085982f2213ba2d4740613e9
daniilscherbina <143995662+daniilscherbina@users.noreply.github.com>
2025-03-12 09:57:43 +0300

    Update 6.sh

--- bash/6.sh
+++ bash/6.sh
@@ -5,12 +5,12 @@ if [[ "$FOO" -eq 5 && "$BAR" -eq 1 ]]; then
 fi
 count=0
 
-timeout -k 5 15s while [ -z "$(ls -A .)" ]; do
+timeout -k 5 15s bash -c 'while [ -z "$(ls -A .)" ]; do
     sleep 1
     count=$(($count+1))
     if [ $count -eq 100 ]; then
         exit 1
     fi
-done
+done'
 
 echo "File"
