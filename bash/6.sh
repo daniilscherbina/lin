422de6a9d7e19c80540545b417fc3eb587111b4e
Scherbina Daniil <you@example.com>
2025-03-15 09:43:07 +0300

    test

--- bash/6.sh
+++ bash/6.sh
@@ -5,12 +5,8 @@ if [[ "$FOO" -eq 5 && "$BAR" -eq 1 ]]; then
 fi
 count=0
 
-while [ -z "$(ls -A .)" ]; do
+while [ -z "fix.txt" ]; do
     sleep 10
-    count=$(($count+1))
-    if [ $count -eq 100 ]; then
-        exit 1
-    fi
 done
 
 echo "File"
