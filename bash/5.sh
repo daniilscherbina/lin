17355df6a2177d1ef51778160576e4b8b2fc4d2c
Scherbina Daniil <you@example.com>
2025-03-15 09:54:08 +0300

    5

--- bash/5.sh
+++ bash/5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for file in $(find "/var/log" -type f -regex '[^\.]+\.log$'); do
+for file in $(find "/var/log" -type f -regex '.*\.log$'); do
     end_st=$(tail -n 1 $file)
     if [ -z "${end_st}" ]; then
-        end_st=$(tail -n 2 $file | tr '\n' '')
+        end_st=$(tail -n 1 $file)
     fi
     echo "$end_st" >> logs.log
 done
