e010425528276fc1fc9fdf69e4fead28469713be
Scherbina Daniil <you@example.com>
2025-05-16 20:28:26 +0300

    fix?

--- docker/src/config.py
+++ docker/src/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: str
-    db_name: str
+    db_user: str = Field(..., env="DB_USER")
+    db_password: str = Field(..., env="DB_PASSWORD")
+    db_host: str = Field(..., env="DB_HOST")
+    db_port: str = Field(..., env="DB_PORT")
+    db_name: str = Field(..., env="DB_NAME")
 
     class Config:
         env_file = ".env"
