1f9c1e607af679cdd16faa12e604408efa3c87ad
Scherbina Daniil <you@example.com>
2025-05-24 10:11:29 +0300

    remove args

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-CONTAINER_NAME="kubsu-container"
-IMAGE="docker.io/deploy320/app:latest"
+if [ $# -ne 6 ]; then
+  echo "Usage: $0 <image_tag>
+  exit 1
+fi
 
-echo "Updating container"
+TAG=$1 
+
+CONTAINER_NAME="kubsu-container"
+IMAGE="docker.io/deploy320/app:latest-$TAG"
+
+echo "Updating container with tag: $TAG"
+echo "Database connection settings:"
+echo " - Host: $DB_HOST"
+echo " - Port: $DB_PORT"
+echo " - DB Name: $DB_NAME"
+echo " - User: $DB_USER"
 
 podman pull $IMAGE || { echo "Failed to pull image"; exit 1; }
 
